d1d574d3d89e8ad4a6d64a044fce339df58d6c8a
runaround <runaroundontheground@gmail.com>
2025-03-03 10:00:27 -0700

    insert data to db from root/courseData.csv automatically

--- server/server.js
+++ server/server.js
@@ -7,14 +7,68 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.RENDER ? process.env.DATABASE_URL_INTERNAL : process.env.DATABASE_URL_EXTERNAL,
+  connectionString: process.env.RENDER
+    ? process.env.DATABASE_URL_INTERNAL
+    : process.env.DATABASE_URL_EXTERNAL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-
 const PORT = process.env.PORT || 3001;
+
+const fs = require("fs");
+const { pipeline } = require("stream");
+const copyFrom = require("pg-copy-streams").from;
+
+async function copyCSVToDB() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(`
+      SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name = 'courses'
+      );
+    `);
+    const tableExists = result.rows[0].exists;
+    if (!tableExists) {
+      // create courses table if it doesn't exist
+      await client.query(`
+      CREATE TABLE IF NOT EXISTS courses (
+        string_id VARCHAR PRIMARY KEY,
+        title VARCHAR,
+        description TEXT,
+        schedule VARCHAR,
+        classroom_number VARCHAR,
+        maximum_capacity INT,
+        credit_hours INT,
+        tuition_cost DECIMAL
+      );
+    `);
+      const query = `COPY courses(string_id, title, description, schedule, classroom_number, maximum_capacity, credit_hours, tuition_cost) 
+                   FROM STDIN WITH CSV HEADER`;
+
+      const stream = client.query(copyFrom(query));
+      const fileStream = fs.createReadStream("../courseData.csv");
+
+      pipeline(fileStream, stream, (err) => {
+        if (err) {
+          console.error("Error inserting CSV:", err);
+        } else {
+          console.log("CSV data inserted successfully!");
+        }
+        client.release();
+      });
+    } else {
+      console.log("courses table already exists");
+    }
+  } catch (error) {
+    console.error("Error inserting CSV:", error);
+    client.release();
+  }
+}
+copyCSVToDB();
 
 const app = express();
 
