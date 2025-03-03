ed37792d05614e0123aa5007f27e6bea9951618c
runaround <runaroundontheground@gmail.com>
2025-03-03 08:54:55 -0700

    added connection to render postgres, uses internal or external database url

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.RENDER ? process.env.DATABASE_URL_INTERNAL : process.env.DATABASE_URL_EXTERNAL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +20,9 @@ const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+// allow json stuff
+app.use(express.json());
 
 // allow cross origin resource sharing
 app.use(cors());
@@ -20,8 +33,19 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({message: "test test test test"})
-})
+  res.json({ message: "test test test test" });
+});
+
+// Test DB Connection
+app.get("/api/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ message: "Connected to DB!", time: result.rows[0].now });
+  } catch (error) {
+    console.error("DB Connection Error:", error);
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
